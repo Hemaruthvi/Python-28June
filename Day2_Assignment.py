e4522c64c3da481d777bf64a106fd3d0129cbbdd
Hemaruthvi <86719826+Hemaruthvi@users.noreply.github.com>
2021-07-18 04:44:38 +0530

    Update Day2_Assignment.py

--- Day2_Assignment.py
+++ Day2_Assignment.py
@@ -1,5 +1,5 @@
 # Type Conversions
-str(), int(), float(), bool()
+#str(), int(), float(), bool()
 
 a = 2727
 print(a, type(a))
@@ -12,6 +12,13 @@ print(b, type(b))
 
 b = str(a)
 print(b, type(b))
+
+# Output:
+
+# 	2727 <class 'int'>
+# 	2727.0 <class 'float'>
+# 	True <class 'bool'>
+# 	2727 <class 'str'>
 
 #============================
 
@@ -27,6 +34,13 @@ print(d, type(d))
 d = bool(c)
 print(d, type(d))
 
+# Output:
+
+# 	1234 <class 'str'>
+# 	1234 <class 'int'>
+# 	1234.0 <class 'float'>
+# 	True <class 'bool'>
+
 #============================
 
 num1 = 345.60
@@ -41,6 +55,12 @@ print(num2, type(num2))
 num2 = str(num1)
 print(num2, type(num2))
 
+# Output:
+
+# 	345.6 <class 'float'>
+# 	345 <class 'int'>
+# 	True <class 'bool'>
+# 	345.6 <class 'str'>
 
 
 #============================
@@ -57,8 +77,12 @@ print(y, type(y))
 y = float(x)
 print(y, type(y))
 
-y = bool(x)
-print(y, type(y))
+# Output:
+
+# 	True <class 'bool'>
+# 	True <class 'str'>
+# 	1 <class 'int'>
+# 	1.0 <class 'float'>
 
 #============================
 
@@ -73,3 +97,32 @@ print(z1, type(z1))
 
 z1 = str(z)
 print(z1, type(z1))
+
+# Output:
+
+	# 0 <class 'int'>
+	# False <class 'bool'>
+	# 0.0 <class 'float'>
+	# 0 <class 'str'>
+
+#===========================
+
+a1 = "Hema"
+print(a1, type(a1))
+
+a2 = int(a1)
+print(a2, type(a2))
+
+a2 = float(a1)
+print(a2, type(a2))
+
+a2 = bool(a1)
+print(a2, type(a2))
+
+# Error: 
+
+# Hema <class 'str'>
+# Traceback (most recent call last):
+#   File "Day2_Assignment.py", line 108, in <module>
+#     a2 = int(a1)
+# ValueError: invalid literal for int() with base 10: 'Hema'
