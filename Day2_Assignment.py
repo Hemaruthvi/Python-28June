246831506d7647e24734b1094794549382defb4b
Hemaruthvi <86719826+Hemaruthvi@users.noreply.github.com>
2021-07-18 04:15:09 +0530

    Add files via upload

---
+++ Day2_Assignment.py
@@ -0,0 +1,75 @@
+# Type Conversions
+str(), int(), float(), bool()
+
+a = 2727
+print(a, type(a))
+
+b = float(a)
+print(b, type(b))
+
+b = bool(a)
+print(b, type(b))
+
+b = str(a)
+print(b, type(b))
+
+#============================
+
+c = "1234"
+print(c, type(c))
+
+d = int(c)
+print(d, type(d))
+
+d = float(c)
+print(d, type(d))
+
+d = bool(c)
+print(d, type(d))
+
+#============================
+
+num1 = 345.60
+print(num1, type(num1))
+
+num2 = int(num1)
+print(num2, type(num2))
+
+num2 = bool(num1)
+print(num2, type(num2))
+
+num2 = str(num1)
+print(num2, type(num2))
+
+
+
+#============================
+
+x = True
+print(x, type(x))
+
+y = str(x)
+print(y, type(y))
+
+y = int(x)
+print(y, type(y))
+
+y = float(x)
+print(y, type(y))
+
+y = bool(x)
+print(y, type(y))
+
+#============================
+
+z = 000
+print(z,type(z))
+
+z1 = bool(z)
+print(z1, type(z1))
+
+z1 = float(z)
+print(z1, type(z1))
+
+z1 = str(z)
+print(z1, type(z1))
